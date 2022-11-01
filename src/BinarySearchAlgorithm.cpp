3d42032165a5ecf9797d3651c5697e99d6016751
Daniel Zajaczkowski <daniel.zajac98@gmail.com>
2022-11-01 12:31:41 +0100

    BinarySearch algorithm fixed

--- src/BinarySearchAlgorithm.cpp
+++ src/BinarySearchAlgorithm.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <vector>
 
-int search(const std::vector<int>& data, int target, int index)
+int search(const std::vector<int>& data, int target, int index_min, int index_max)
 {
-    if (data[index-1] == target)
+    int mid = (index_min+index_max)/2;
+    if (data[mid-1] == target)
     {
-        return index;
+        return mid;
     }
-    else if (data[index-1] > target)
+    else if (data[mid-1] > target)
     {
-        std::cout << "Search left " << index << std::endl;
-        return search(data, target, ((index%2 == 0) ? index-(index/2) : index-(index/2)+1));
+        return search(data, target, index_min, mid);
 
     }
-    else if (data[index-1] < target)
+    else if (data[mid-1] < target)
     {
-        std::cout << "Search right " << index << std::endl;
-        return search(data, target, ((index+(index/2)%2 == 0) ? index+(index/2)-1 : index+(index/2)));
+        return search(data, target, mid, index_max);
     }
 
     return -1;
@@ -24,9 +23,9 @@ int search(const std::vector<int>& data, int target, int index)
 
 int main()
 {
-    std::vector<int> data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+    std::vector<int> data = {1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48};
 
-    const auto index = search(data, 10, data.size()/2);
+    const auto index = search(data, 16, 0, data.size());
 
     std::cout << index << std::endl;
     return 0;
